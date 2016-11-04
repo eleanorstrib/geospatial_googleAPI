b6dda6eb973923be0a1d6261e9138248c472fe64
eleanorstrib <eleanor@sloan.mit.edu>
2016-11-04 11:01:33 -0700

    url constructed

--- test_geocode.py
+++ test_geocode.py
@@ -6,8 +6,7 @@ API_KEY = os.environ['GOOGLE_API_KEY']
 file_reader=csv.reader(open('sample.csv', 'rU'), dialect='excel')
 f
 or row in file_reader:
-	street_address = row[4].replace(' ', '+')
-	city, state, zip_code = row[5], row[6], row[7]
+	street_address, city, state, zip_code = (row[5], row[6], row[7]).replace(' ', '+')
 	print (street_address, city, state, zip_code)
 	URL = 'https://maps.googleapis.com/maps/api/geocode/json?address={}+{}+{}&key={}'.format(street_address, city, state, API_KEY)
 	print(URL)
